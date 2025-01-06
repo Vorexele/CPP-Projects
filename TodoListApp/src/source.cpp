24a6bcfa25674ff552e116f08a3e14d427190066
vorexele <ali.alaskarovdev@gmail.com>
2025-01-06 23:21:02 +0100

    Initial commit

---
+++ TodoListApp/src/source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int a = 5, b = 10;
+
+    int result = a + b;
+
+    std::cout << "Hello, World" << std::endl;
+
+    printf("The result is: %d\n", result);
+
+    return 0;
+}
